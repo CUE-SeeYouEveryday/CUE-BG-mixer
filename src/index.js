efecaa04297e5ccdab4921aaf9c9775d5a2683d6
CPK <samkit157@gmail.com>
2024-04-12 15:46:26 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -4,26 +4,34 @@ import { BgContainer } from './bgcontainer.js'
 import * as fs from 'fs';
 import * as path from 'path';
 
+const InputDir = './bg'
+const outputDir = './out/'
+
+async function takeScreenshot(app, filename){
+    app.renderer.render(app.stage);
+    const base64Image = app.renderer.view.toDataURL('image/png');
+    const base64Data = base64Image.replace(/^data:image\/png;base64,/, '');
+    const output = path.join(outputDir, `./${filename}.png`);
+
+    console.log(`Export : ${filename}`)
+    fs.writeFileSync(output, base64Data, 'base64');
+}
+
 const app = new Application({
     width: 1334,
     height: 750,
 });
 
+const folderObj = fs.readdirSync(InputDir);
 
+for (let file of folderObj){
+    const bg = await BgContainer.from(path.join(InputDir, file, 'manifest.json'))
+    app.stage.addChild(bg);
 
-const bg = await BgContainer.from('./bg/background004_1/manifest.json')
-app.stage.addChild(bg);
+    takeScreenshot(app, file);
 
-
-app.renderer.render(app.stage);
-const base64Image = app.renderer.view.toDataURL('image/png');
-const base64Data = base64Image.replace(/^data:image\/png;base64,/, '');
-const output = `./background004_1.png`;
-
-fs.writeFileSync(output, base64Data, 'base64');
-
-bg.destroy(true)
-
-console.log(Assets.cache._cacheMap)
+    bg.destroy({texture : true, baseTexture : true, children : true})
+    app.stage.removeChildren();
+}
 
 process.exit(0);
